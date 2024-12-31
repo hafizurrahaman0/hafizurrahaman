b7cf752690e4f0a164adf77671c95f2a36e4b1dd
hafizurrahaman0 <166695318+hafizurrahaman0@users.noreply.github.com>
2024-12-31 10:19:17 +0530

    Changed the card design in the project section

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@ document.querySelector('.menu-toggle').addEventListener('click', function () {
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
+        console.log(e);
 
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
